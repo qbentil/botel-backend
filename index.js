7b34ed9608184ebd924bc46fbcc8680e794e4856
qbentil <bentilshadrack72@gmail.com>
2022-06-27 10:45:08 +0000

    COnnect DB

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+import dotenv from 'dotenv'
 import express from 'express'
+import mongoose from 'mongoose'
 
 const APP = express();
+dotenv.config();
+
+
+const DBCONNECT  = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        console.log('DB Connected');
+      } catch (error) {
+        console.log(error);
+      }
+}
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB Connected');
+})
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB Disconnected');
+})
 
 APP.listen(8800, () =>{
-    console.log('Server is running on port 8800');
+    DBCONNECT();
+    console.log('Server is running on port 8800......');
 })
