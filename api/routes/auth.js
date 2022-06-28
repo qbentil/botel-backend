a75f29ab29c66403071a245ed3472962ecb92cb6
qbentil <bentilshadrack72@gmail.com>
2022-06-28 11:33:03 +0000

    Add user login route to Auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,10 @@
+import { login, register } from '../controllers/auth.js';
+
 import express from 'express';
-import { register } from '../controllers/auth.js';
 
 const router = express.Router();
 
 router.post('/register', register)
+router.post('/login', login)
 
 export default router;
