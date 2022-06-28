4308e303e0c7e9572fd7e45aac73deceb912c9a9
qbentil <bentilshadrack72@gmail.com>
2022-06-28 10:37:40 +0000

    Update Hotel Model

---
+++ api/models/Hotel.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const HotelSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    stars: {
+        type: Number,
+        min: 0,
+        max: 5,
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    photos: {
+        type: [String],
+
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    distance: {
+        type: Number,
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    rooms:{
+        type: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Room'
+        }]
+    },
+    cheapestPrice: {
+        type: Number,
+        required: true
+    }
+})
+
+export default mongoose.model('Hotel', HotelSchema);
