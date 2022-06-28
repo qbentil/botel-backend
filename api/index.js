f19759764e7b9274f118ba6ab6d03df903f1dac1
qbentil <bentilshadrack72@gmail.com>
2022-06-28 06:35:44 +0000

    Create error hadling middleware

--- api/index.js
+++ api/index.js
@@ -33,6 +33,19 @@ APP.use('/api/rooms', roomsRoute);
 APP.use('/api/users', usersRoute);
 APP.use('/api/auth', authRoute);
 
+// error handling MIDDLEWARE
+APP.use((err, req, res, next) => {
+    console.log("Middleware Error Hadnling");
+    const errStatus = err.statusCode || 500;
+    const errMsg = err.message || 'Something went wrong';
+    res.status(errStatus).json({
+        success: false,
+        status: errStatus,
+        message: errMsg,
+        stack: err.stack
+    })
+})
+
 
 
 
