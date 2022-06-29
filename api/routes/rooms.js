e450dea12631375858dca0815f5aec87aeea14dd
qbentil <bentilshadrack72@gmail.com>
2022-06-29 13:04:17 +0000

    Update rooms route

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,9 +1,22 @@
+import { createRoom, deleteRoom, getRoomById, getRooms, updateroom } from '../controllers/room.js';
+import { verifyAdmin, verifyUser } from '../utils/verifyToken.js';
+
 import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('rooms Endpoint');
-})
+// CREATE
+router.post("/:hotelId", createRoom);
+
+// UPDATE
+router.put("/:id", verifyAdmin, updateroom);
+// DELETE
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom)
+
+// Rooms
+router.get("/", getRooms)
+
+// Room/:ID
+router.get("/:id",   getRoomById)
 
 export default router;
